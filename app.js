64369506ab471874af4053624b4d02a8c6c1d930
Mohamed Sihan <39284618+Mshihan@users.noreply.github.com>
2021-09-26 19:45:54 +0530

    Crud requests implementation

--- app.js
+++ app.js
@@ -35,6 +35,27 @@ app.get("/api/v1/tours", (req, res) => {
   });
 });
 
+// ======================================
+// Get request to get a individual tour
+// ======================================
+app.get("/api/v1/tours/:id", (req, res) => {
+  console.log(req.params);
+
+  const id = req.params.id * 1;
+  const tour = tours.find((el) => el.id === id);
+
+  console.log(tour);
+  if (!tour) {
+    return res.status(404).send("Tour not found");
+  }
+  res.status(200).json({
+    status: "success",
+    data: {
+      tour,
+    },
+  });
+});
+
 // ===============================
 // Post request to send all tours
 // ===============================
@@ -58,6 +79,34 @@ app.post("/api/v1/tours", (req, res) => {
       newTour,
     },
   });
+});
+
+// =================================
+// Patch request to update the tours
+// =================================
+app.patch("/api/v1/tours/:id", (req, res) => {
+  const tourId = req.params.id;
+
+  if (tourId > tours.length) {
+    return res.status(404).json({ status: "fail", message: "Tour not found" });
+  }
+
+  res
+    .status(200)
+    .json({ status: "success", data: "<Updated tour details here>" });
+});
+
+// =================================
+// Patch request to update the tours
+// =================================
+app.delete("/api/v1/tours/:id", (req, res) => {
+  const tourId = req.params.id;
+
+  if (tourId > tours.length) {
+    return res.status(404).json({ status: "fail", message: "Tour not found" });
+  }
+
+  res.status(204).json({ status: "success", data: null });
 });
 
 const port = 3000;
