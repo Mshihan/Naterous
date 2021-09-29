b176dd76161fbde5c9c49ceaab92a9448b7bc5ba
Mohamed Sihan <39284618+Mshihan@users.noreply.github.com>
2021-09-29 11:55:51 +0530

    configured the mongoose and created mongoose data models

--- server.js
+++ server.js
@@ -1,7 +1,37 @@
-const app = require("./app");
 const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const app = require("./app");
 
 dotenv.config({ path: "./config.env" });
+
+const DB = process.env.DATABASE.replace(
+  "<PASSWORD>",
+  process.env.DB_PASSWORD
+);
+
+mongoose.connect(DB).then(() => {
+  console.log("Successfuly connected to database.....");
+});
+
+// Mongoose Schema Configuration
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "A tour must have a name"],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, "A tour must have a price"],
+  },
+});
+
+// Mongoose Model
+const Tour = mongoose.model("Tour", tourSchema);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
