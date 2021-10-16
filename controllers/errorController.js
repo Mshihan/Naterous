90f976b3f21d515532352e324dea3db5dc19f45d
Mohamed Sihan <39284618+Mshihan@users.noreply.github.com>
2021-10-17 00:02:41 +0530

    Error refactoring and reformating for production

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,10 +1,35 @@
+const sendErrorProd = (err, res) => {
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  } else {
+    console.log("Something went wrong 💥", err);
+    res.status(500).json({
+      status: "error",
+      message: "Something went very wrong!",
+    });
+  }
+};
+
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+    error: err,
+    statusCode: err.statusCode,
+  });
+};
+
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || "error";
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  if (process.env.NODE_ENV === "production") {
+    sendErrorProd(err, res);
+  } else if (process.env.NODE_ENV === "development") {
+    sendErrorDev(err, res);
+  }
   next();
 };
